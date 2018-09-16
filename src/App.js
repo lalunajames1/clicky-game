fa5b144be8d832e027325f887e114406943dde14
Luis Luna <luisluna@GHC-SEO-0032.globalhc.io>
2018-09-16 14:38:06 -0500

    something

--- src/App.js
+++ src/App.js
@@ -1,6 +1,10 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import Board from "./components/board";
+import Footer from "./components/footer";
+import Images from "./components/images";
+
+import simpsons from "./simpsons.json";
+import "./App.css";
 
 class App extends Component {
   render() {
